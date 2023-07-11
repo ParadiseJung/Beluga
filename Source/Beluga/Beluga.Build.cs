fb3ef71679dd7323a38aaf7e711a1bf44c30550a
winkey83 <winkey83@gmail.com>
2023-07-12 00:31:42 +0900

    add project files

---
+++ Source/Beluga/Beluga.Build.cs
@@ -0,0 +1,13 @@
+using UnrealBuildTool;
+
+public class Beluga : ModuleRules
+{
+	public Beluga(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+	}
+}
